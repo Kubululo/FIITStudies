5365dc0fd4e682da3c76f55c2b6734216979a641
Jakub Skurčák <skurcak@gmail.com>
2019-09-30 17:41:30 +0200

    Edit few files

--- Základy proceduralneho programovania/SecondAlgorithm 30.09.2019.c
+++ Základy proceduralneho programovania/SecondAlgorithm 30.09.2019.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#define _USE_MATH_DEFINES
-#include <math.h>
 
 
 int main(){
 float r;
 printf("Zadaj polomer");
 scanf("%f",&r);
-printf("Obsah kruhu je :%f\n Obvod kruhu je %f",2*3.14*r,3.14*r*r);
+printf("Obsah kruhu je :%.2f\n Obvod kruhu je %.2f",2*3.14*r,3.14*r*r);
 return 0;
 }
--- Základy proceduralneho programovania/ThirdAlgorithm 30.09.2019.c
+++ Základy proceduralneho programovania/ThirdAlgorithm 30.09.2019.c
@@ -4,6 +4,6 @@ int main(){
     float p;
     printf("Zadaj cenu bez DPH:\n");
     scanf("%f",&p);
-    printf("Cena s DPH je %f",p*0.2+p);
+    printf("Cena s DPH je %.1f",p*0.2+p);
     return 0;
 }
